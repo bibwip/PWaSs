8ae744039c332f3fe1dc9fba876f8208915f3228
nolan <nolanhilbersgarcia@gmail.com>
2023-01-08 13:52:56 +0100

    kleuren in prefs bewaard

--- app/src/main/java/com/thethreewisemen/pwass/MainActivity.kt
+++ app/src/main/java/com/thethreewisemen/pwass/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.thethreewisemen.pwass
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 
+
 class MainActivity : AppCompatActivity() {
+
+    var hasCustomTheme = false
+    var colorPrimary = ""
+    var colorPrimaryVariant = ""
 
     private lateinit var navController : NavController
 
@@ -17,13 +26,33 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_graph_container) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+        prefs.edit().putBoolean(HASCUSTOMTHEME, true).apply()
+        prefs.edit().putString(COLORPRIMARY, "#34eb3d").apply()
+        prefs.edit().putString(COLORPRIMARYVAR, "#eb34d8").apply()
+        if (prefs.getBoolean(HASCUSTOMTHEME, false)) {
+            hasCustomTheme = true
+            Log.d(TAG, "has custom theme")
+            setColors(prefs)
+            Log.d(TAG, "prim col: $colorPrimary")
+        }
+        checkFirstRun(prefs)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor(colorPrimary)))
+        window.statusBarColor = Color.parseColor(colorPrimaryVariant)
+
     }
 
-    private fun checkFirstRun() {
+    private fun setColors(prefs: SharedPreferences){
+        colorPrimary = prefs.getString(COLORPRIMARY, "#eb4034")!!
+        colorPrimaryVariant = prefs.getString(COLORPRIMARYVAR, "#eb4034")!!
+
+    }
+
+    private fun checkFirstRun(prefs : SharedPreferences) {
         // pakt de huidige versie en de bewaarde versie codes
         val currentVersionCode = BuildConfig.VERSION_CODE
-        val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
-        val savedVersionCode = prefs!!.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST)
+        val savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST)
 
         if (currentVersionCode == savedVersionCode) {
             // niks nieuws
@@ -37,10 +66,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        const val TAG = "epic"
+
         const val PREF_VERSION_CODE_KEY = "version_code"
         const val DOESNT_EXIST = -1
         const val PREFS_NAME = "MyPrefsFile"
-        const val TAG = "MAIN"
+
+        private const val HASCUSTOMTHEME = "isCustomTheme"
+        private const val COLORPRIMARY = "colorPrimary"
+        private const val COLORPRIMARYVAR = "colorPrimaryVar"
+
     }
 
 }
--- app/src/main/java/com/thethreewisemen/pwass/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/thethreewisemen/pwass/fragments/WelcomeFragment.kt
@@ -1,27 +1,30 @@
 package com.thethreewisemen.pwass.fragments
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import com.thethreewisemen.pwass.R
 import androidx.navigation.findNavController
+import com.thethreewisemen.pwass.MainActivity
 
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.testButton)
+        val main = (activity as MainActivity)
+
+        if (main.hasCustomTheme) {
+            button.background.setTint(Color.parseColor(main.colorPrimary))
+        }
 
         // Druk op de knop stuurt je door naar volgende bestemming
-        view.findViewById<Button>(R.id.testButton).setOnClickListener {
+        button.setOnClickListener {
             val action = WelcomeFragmentDirections.actionWelcomeFragmentToVolgendeFrag()
             view.findNavController().navigate(action)
         }
