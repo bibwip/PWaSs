7b742b4b56e3f5eefac79088f297b5f009992007
nolan <nolanhilbersgarcia@gmail.com>
2023-01-10 10:28:18 +0100

    addPost werkt nu

--- app/src/main/java/com/thethreewisemen/pwass/fragments/MainFragment.kt
+++ app/src/main/java/com/thethreewisemen/pwass/fragments/MainFragment.kt
@@ -3,8 +3,11 @@ package com.thethreewisemen.pwass.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.thethreewisemen.pwass.R
 import com.thethreewisemen.pwass.adapters.RecyclerAdapter
 import com.thethreewisemen.pwass.firestore.getPosts
@@ -17,9 +20,19 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         val adapter = RecyclerAdapter(arrayListOf())
         val recycler = view.findViewById<RecyclerView>(R.id.recyclerPosts)
+        val button = view.findViewById<FloatingActionButton>(R.id.mainAddPostBtn)
+        val refresh = view.findViewById<Button>(R.id.mainRefreshBtn)
         recycler.layoutManager = LinearLayoutManager(context)
         recycler.adapter = adapter
         getPosts(adapter)
+
+        button.setOnClickListener {
+            val action = MainFragmentDirections.actionMainFragmentToAddPostFragment()
+            view.findNavController().navigate(action)
+        }
+        refresh.setOnClickListener {
+            getPosts(adapter)
+        }
 
 
 
